3247c1a031e800adb68038281eb7e1a74d0cfbbd
Edson C <edsoncelio@users.noreply.github.com>
2022-08-02 23:21:37 -0300

    feat: add updated script

--- glossary_diff.py
+++ glossary_diff.py
@@ -16,12 +16,15 @@ def clone_repo(branch, uri):
 
 def get_uncompleted(upstream_path):
     """get uncompleted definitions"""
-    uncompleted= []
+    uncompleted = []
+    out_uncompleted = []
     cmd_diff = ["grep", "-r", "status: Feedback Appreciated", upstream_path]
     try:
         out = subprocess.run(cmd_diff, stdout=PIPE)
         uncompleted = (out.stdout.decode('ascii')).splitlines()
-        return uncompleted
+        for line in uncompleted:
+            out_uncompleted.append(line.split(":")[0])
+        return out_uncompleted
     except subprocess.CalledProcessError as exception:
         return exception
 
@@ -36,24 +39,25 @@ def diff_metadata():
         for item in out_list:
             if "Files " in item:
                 diff_dev.append(item)
-            if "Only in" in item:
+            if "Only in" in item and item:
                 only_upstream.append(item)
         return only_upstream, diff_dev
     except subprocess.CalledProcessError as exception:
         return exception
 
 
-def diff_repo():
+def diff_repo(upstream_path):
     """diff repos"""
     only_upstream = []
     diff_dev = []
+    uncompleted_files = get_uncompleted(upstream_path)
     cmd_diff = ["diff", "--brief", "upstream/content/en", "dev-pt/content/en"]
 
     try:
         out = subprocess.run(cmd_diff, stdout=PIPE)
         out_list = (out.stdout.decode('ascii')).splitlines()
         for item in out_list:
-            if "Files " in item:
+            if "Files " in item and item.split(" ")[1] not in uncompleted_files:
                 diff_dev.append(item)
             if "Only in" in item:
                 only_upstream.append(item)
@@ -65,25 +69,17 @@ def diff_repo():
 if __name__ == "__main__":
     REPO_URI = "https://github.com/cncf/glossary.git"
     BRANCH_DEV = "dev-pt"
-    METADATA_LIST = ["upstream/content/en/style-guide/_index.md", "upstream/content/en/_TEMPLATE.md"]
+    METADATA_LIST = ["upstream/content/en/style-guide/_index.md", "upstream/content/en/_TEMPLATE.md", "upstream/content/en/style-guide/_index.md","upstream/content/en/contributor-ladder/_index.md" ]
     clone_repo(BRANCH_DEV, REPO_URI)
-    out_diff = diff_repo()
-    print("## Updated content")
+    out_diff = diff_repo("upstream/content/en")
+
+    print("English updated content: ")
     for line in out_diff[1]:
         print(line.split(" ")[1])
     print("\n")
 
-    print("## Updated metadata")
+    print("Updated metadata: ")
     out_diff_mt = diff_metadata()
     for line in out_diff_mt[1]:
         print(line.split(" ")[1])
     print("\n")
-    
-    # print("Uncompleted files - status: Feedback Appreciated: ")
-    # out_uncompleted = get_uncompleted("upstream/content/en")
-    # for line in out_uncompleted:
-    #     if line.split(":")[0] not in METADATA_LIST:
-    #         print(line.split(":")[0])
-    # print("\n")
-        
-
